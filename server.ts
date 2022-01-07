ddf49bd04a7a77d4106429a923d34a5daac42764
gelatina <santiagozapatabohorquez@hotmail.com>
2022-01-07 13:03:49 -0500

    added tsconfig

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+import Shopify, { ApiVersion as Version, AuthQuery, DataType } from '@shopify/shopify-api';
 var express = require('express');
 var app = express();
 const bodyParser = require("body-parser");
@@ -30,6 +31,7 @@ function formatDate(date) {
 
 axios.get(APIUrl)
   .then(function (response) {
+    // handle success
     const products = response.data.products
 
     products.map((item) => {
@@ -44,16 +46,27 @@ axios.get(APIUrl)
         }
       )
     })
+
+    // console.log(products);
     console.log(productList);
   })
   .catch(function (error) {
+    // handle error
     console.log(error);
   })
+  .then(function () {
+    // always executed
+  });
 
 
 app.get("/", async (req, res) => {
   res.json(productList)
 });
+
+
+
+// console.log(API_KEY)
+// console.log(API_SECRET_KEY)
 
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
