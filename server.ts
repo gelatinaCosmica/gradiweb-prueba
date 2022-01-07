85917b5b08fd495cdda761d5d7d2c1d4f11f1e91
gelatina <santiagozapatabohorquez@hotmail.com>
2022-01-07 12:49:07 -0500

    refactor code

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-import Shopify, { ApiVersion as Version, AuthQuery } from '@shopify/shopify-api';
+import Shopify, { ApiVersion as Version, AuthQuery, DataType } from '@shopify/shopify-api';
 var express = require('express');
 var app = express();
 const bodyParser = require("body-parser");
@@ -8,56 +8,51 @@ const axios = require('axios').default;
 
 const { API_KEY, API_SECRET_KEY, SCOPES, SHOP, HOST } = process.env;
 
-const APIUrl = `https://${process.env.SHOP}.myshopify.com/admin/api/2022-01/products.json`
-
-
-const ApiVersion = {
-  version: "2021-10"
-}
-
-Shopify.Context.initialize({
-  API_KEY,
-  API_SECRET_KEY,
-  SCOPES: ["products"],
-  HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
-  IS_EMBEDDED_APP: true,
-  API_VERSION: Version.October21 // all supported versions are available, as well as "unstable" and "unversioned"
-})
-
-const ACTIVE_SHOPIFY_SHOPS: { [key: string]: string | undefined } = {};
+const APIUrl = `https://${API_KEY}:${API_SECRET_KEY}@${SHOP}/admin/api/2022-01/products.json`
 
 app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(bodyParser.json());
 
+let productList = []
+
 axios.get(APIUrl)
   .then(function (response) {
-      // handle success
-      console.log(response);
+    // handle success
+    const products = response.data.products
+
+    products.map((item) => {
+      productList.push(
+        {
+          title: item.title,
+          data: {
+            price: item.variants[0].price,
+            status: item.status,
+            created_at: item.created_at,
+          }
+        }
+      )
     })
-    .catch(function (error) {
+
+    // console.log(products);
+    console.log(productList);
+  })
+  .catch(function (error) {
     // handle error
     console.log(error);
   })
   .then(function () {
-      // always executed
-    });
-  
-  app.get("/", async (req, res) => {
-  const session = await Shopify.Utils.loadCurrentSession(req, res);
-  const client = new Shopify.Clients.Rest(session.shop, session.accessToken);
-  const response = client.get({path: 'shop'});
+    // always executed
+  });
 
-  if (ACTIVE_SHOPIFY_SHOPS[SHOP] === undefined) {
-    // not logged in, redirect to login
-    res.redirect(`/login`);
-  } else {
-    res.send("Hello world!");
-    // Load your app skeleton page with App Bridge, and do something amazing!
-    res.end();
-  }
+
+app.get("/", async (req, res) => {
+  res.json(productList)
 });
 
-// console.log(process.env.API_KEY)
+
+
+// console.log(API_KEY)
+// console.log(API_SECRET_KEY)
 
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
